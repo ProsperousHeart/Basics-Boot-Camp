5a0961b2249ef545e2008e53e697af9c1e9f7c66
Kassandra Keeton <ProsperousHeart@users.noreply.github.com>
2021-11-27 11:28:32 -0600

    Completion of calc HW for week 1

--- HW/Student_Files/prosperousheart/W1H1-Calc.py
+++ HW/Student_Files/prosperousheart/W1H1-Calc.py
@@ -9,8 +9,7 @@ from command prompt, a certain action will take place.
 
 This script will require that module Math be installed.
 However, the initial phase of this will be uploaded to GitHub
-with just the bare code as per this training:
-
+with just the bare code -no functions or exception handling.
 
 This file can not yet be imported as a module with functions
 - functions are covered in week 2.
@@ -40,13 +39,69 @@ if __name__ == "__main__":
         print("4 - subtract numbers")
         print("5 - divide numbers")
 
-        # Get Response
-        usr_input = int(input())        # assuming input always INT
+        # ==============================================
+        # Get Response:
+        # assuming input always INT could introduce bugs
+        #           usr_input = int(input())
+        # ==============================================
+
+        usr_input = input()
 
         # Compare User Input & Respond Appropriately
-        if usr_input == 1:
-            not_done = False
-            print("\nThank you, come again! ^_^")
+        if usr_input in ["1", "2", "3", "4", "5"]:
+            # exit
+            if usr_input == "1":
+                not_done = False
+                print("\nThank you, come again! ^_^")
+
+            # add numbers
+            elif usr_input == "2":
+                # consider what you would need to do if more than 2 numbers were desired
+                add1 = input("\nPlease provide your 1st of 2 numbers:\t")
+                add2 = input("Please provide your 2nd of 2 numbers:\t")
+
+                # assuming numbers ONLY provided - will error out and not catch
+                print("\n{} + {} = {}\n".format(add1,
+                                                add2,
+                                                int(add1)+int(add2)))
+
+            # multiply numbers
+            elif usr_input == "3":
+                # consider what you would need to do if more than 2 numbers were desired
+                add1 = input("\nPlease provide your 1st of 2 numbers:\t")
+                add2 = input("Please provide your 2nd of 2 numbers:\t")
+
+                # assuming numbers ONLY provided - will error out and not catch
+                print("\n{} * {} = {}\n".format(add1,
+                                                add2,
+                                                int(add1)*int(add2)))
+
+            # subtract numbers
+            elif usr_input == "4":
+                # consider what you would need to do if more than 2 numbers were desired
+                add1 = input("\nPlease provide your 1st of 2 numbers:\t")
+                add2 = input("Please provide your 2nd of 2 numbers:\t")
+
+                # assuming numbers ONLY provided - will error out and not catch
+                print("\n{} - {} = {}\n".format(add1,
+                                                add2,
+                                                int(add1)-int(add2)))
+
+            # divide numbers
+            elif usr_input == "5":
+                # consider what you would need to do if more than 2 numbers were desired
+                add1 = input("\nPlease provide your 1st of 2 numbers:\t")
+                add2 = input("Please provide your 2nd of 2 numbers:\t")
+
+                # assuming numbers ONLY provided - will error out and not catch
+                print("\n{} / {} = {}\n".format(add1,
+                                                add2,
+                                                int(add1)/int(add2)))
+
+            # once all items programmed, this section will never be used
+            # here to show an option while coding as a "placeholder"
+            else:
+                print("\nWARNING:  Your choice has not yet been scripted. Please choose another.\n")
         else:
             print("\nSorry - we do not recognize your input. Please try again.\n")
 
